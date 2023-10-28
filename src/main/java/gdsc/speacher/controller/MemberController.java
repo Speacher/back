ca357b99db94e89aaec0ba750ae7b753fdc54c39
leesewon00 <dltpdnjs2000@gmail.com>
2023-10-28 14:24:07 +0900

    Feat : Add member crud action

--- src/main/java/gdsc/speacher/controller/MemberController.java
+++ src/main/java/gdsc/speacher/controller/MemberController.java
@@ -1,12 +1,57 @@
 package gdsc.speacher.controller;
 
+import gdsc.speacher.entity.Member;
 import gdsc.speacher.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.Optional;
+
+@RestController
 @RequiredArgsConstructor
 public class MemberController {
 
-    private MemberService memberService;
+    private final MemberService memberService;
+
+    // 회원가입
+    @PostMapping("/members/new")
+    public ResponseEntity createMember(@RequestBody Member member) {
+        Member save = memberService.save(member);
+        if (save == null) {
+            return ResponseEntity.badRequest().body("가입 실패");
+        }
+        return ResponseEntity.ok().body(save);
+    }
+
+    // 로그인
+    @PostMapping("/members/login")
+    public ResponseEntity login(@RequestBody Member member) {
+        Optional<Member> login = memberService.login(member.getEmail(), member.getPassword());
+        if (login.isPresent()) {
+            return ResponseEntity.ok(login.get());
+        }
+        return ResponseEntity.badRequest().body("로그인 실패");
+    }
+
+    // 회원정보 수정
+    @PatchMapping("/members/{id}")
+    public ResponseEntity updateMember(@PathVariable Long id, @RequestBody Member member) {
+        Member update = memberService.update(id, member);
+        if (update == null) {
+            return ResponseEntity.badRequest().body("수정 실패");
+        }
+        return ResponseEntity.ok().body(member);
+    }
+
+    // 회원정보 조회
+    @GetMapping("/members/{id}")
+    public ResponseEntity findById(@PathVariable Long id) {
+        Optional<Member> member = memberService.findById(id);
+        if (member.isPresent()) {
+            return ResponseEntity.ok().body(member.get());
+        } else {
+            return ResponseEntity.badRequest().body("조회 실패");
+        }
+    }
 }
--- src/main/java/gdsc/speacher/service/MemberService.java
+++ src/main/java/gdsc/speacher/service/MemberService.java
@@ -1,13 +1,45 @@
 package gdsc.speacher.service;
 
+import gdsc.speacher.entity.Member;
 import gdsc.speacher.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
 
-    private MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
 
+    public Member save(Member member) {
+        Optional<Member> byEmail = memberRepository.findByEmail(member.getEmail());
+        if (byEmail.isEmpty()) { // 중복 아이디 예외처리
+            memberRepository.save(member);
+            return memberRepository.findByEmail(member.getEmail()).get();
+        } else {
+            return null;
+        }
+    }
+
+    public Optional<Member> login(String email, String password) {
+        return memberRepository.findByEmailAndPassword(email, password);
+    }
+
+    public Member update(Long id, Member member) {
+        Optional<Member> byId = memberRepository.findById(id);
+        if (byId.isPresent()) {
+            byId.get().update(member);
+            return memberRepository.findById(id).get();
+        } else {
+            // 없는 회원
+            return null;
+        }
+    }
+
+    public Optional<Member> findById(Long id) {
+        Optional<Member> byId = memberRepository.findById(id);
+        return byId;
+    }
 }
