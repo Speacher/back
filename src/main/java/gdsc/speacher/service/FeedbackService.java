615a074d323ff4977dd1d2a3498d12d93950951b
leesewon00 <dltpdnjs2000@gmail.com>
2023-10-29 19:44:19 +0900

    Refactor : Feedback error handling

--- src/main/java/gdsc/speacher/service/FeedbackService.java
+++ src/main/java/gdsc/speacher/service/FeedbackService.java
@@ -1,8 +1,11 @@
 package gdsc.speacher.service;
 
 import gdsc.speacher.dto.feedback.FeedbackDto;
+import gdsc.speacher.dto.feedback.VideoDto;
 import gdsc.speacher.entity.Feedback;
+import gdsc.speacher.entity.Video;
 import gdsc.speacher.exception.FeedbackException;
+import gdsc.speacher.exception.MemberException;
 import gdsc.speacher.repository.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,21 +14,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-import static gdsc.speacher.exception.ErrorCode.INVALID_FEEDBACK_ID;
-import static gdsc.speacher.exception.ErrorCode.INVALID_VIDEO_ID;
+import static gdsc.speacher.exception.ErrorCode.*;
 
 @Service
 @RequiredArgsConstructor
 public class FeedbackService {
 
     private final FeedbackRepository feedbackRepository;
+    private final VideoRepository videoRepository;
 
     public FeedbackDto findByFeedbackId(Long id) {
         Optional<Feedback> byId = feedbackRepository.findById(id);
-        if (byId.isEmpty()) {
+        if(!byId.isPresent()){
             throw new FeedbackException(INVALID_FEEDBACK_ID);
         }
-
         Feedback feedback = byId.get();
         FeedbackDto feedbackDto = new FeedbackDto(
                 feedback.getVideo(), feedback.getCv(), feedback.getNlp(),
@@ -35,15 +37,19 @@ public class FeedbackService {
     }
 
     public List<FeedbackDto> findByVideoId(Long id) {
+        Optional<Video> byId = videoRepository.findById(id);
+        if(!byId.isPresent()){
+            throw new FeedbackException(INVALID_VIDEO_ID);
+        }
         Optional<List<Feedback>> all = feedbackRepository.findAllByVideoIdOrderByCreateDateDesc(id);
-        if(all.isEmpty()){
+        if(!all.isPresent()){
             throw new FeedbackException(INVALID_VIDEO_ID);
         }
         List<Feedback> feedbacks = all.get();
         List<FeedbackDto> feedbackDtos = new ArrayList<>();
         for (Feedback feedback : feedbacks) {
             FeedbackDto feedbackDto = new FeedbackDto(
-                    feedback.getVideo(),feedback.getCv(), feedback.getNlp(),
+                    feedback.getVideo(), feedback.getCv(), feedback.getNlp(),
                     feedback.getVr(), feedback.getTitle());
             feedbackDtos.add(feedbackDto);
         }
